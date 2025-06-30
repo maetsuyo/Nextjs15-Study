d902ad4ff008e8510a8237c88c49d6b5c63f5be7
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2025-07-01 02:09:43 +0900

    認証はできたから次はセッションに保存する

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,65 @@
-function Login() {
+"use client"
+
+import { useState } from "react";
+
+type UserProps = {
+  id: number;
+  username: string;
+  password: string;
+}
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:4000/users");
+      const users: UserProps[] = await res.json();
+      const user: UserProps | undefined = users.find((u:UserProps) => u.username === username && u.password === password);
+
+      if (user) {
+        setMessage("ログイン成功");
+      } else {
+        setMessage("ログイン失敗");
+      }
+    } catch (e) {
+      console.error(e);
+      setMessage("エラー");
+    }
+  }
   return (
     <div>
-      <form action="">
-        <p>ユーザーネーム</p>
-        <input type="text" name="username" placeholder="username"/>
+      <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleLogin}>
+        <p>ユーザー名</p>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <p>パスワード</p>
-        <input type="text" name="password" placeholder="password"/>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">
+          ログイン
+        </button>
       </form>
-      <button type="submit">
-        ログイン
-      </button>
+      </div>
+      {message && (
+        <p>{message}</p>
+      )}
     </div>
   );
 }
-
-export default Login;
